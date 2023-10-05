d3afed3542b881f2bc7fa91ae2d9484a7c41beb4
root <root@DESKTOP-M9KRAU5>
2023-10-05 11:29:34 +0545

    initial

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const http = require("http")
+const {Server} = require("socket.io")
+const cors = require("cors")
+
+app.use(cors());
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket)=>{
+    console.log(`User Connected: ${socket.id}`);
+
+    socket.on('Tuna Technology', (message)=>{
+        io.emit('Tuna Technology', message)
+    })
+
+    socket.on('Shubha Om Company Pvt.Ltd', (message)=>{
+        io.emit('Shubha Om Company Pvt.Ltd', message)
+    })
+})
+
+
+server.listen(5000, ()=> {
+    console.log("Server is running...");
+})
